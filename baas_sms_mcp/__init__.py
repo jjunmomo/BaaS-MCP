a83b0d277448a7f805537b578e240b4985850e4a
jjunmomo <jjunmo@mbaas.kr>
2025-08-01 16:20:17 +0900

    update

--- baas_sms_mcp/__init__.py
+++ baas_sms_mcp/__init__.py
@@ -4,7 +4,7 @@ BaaS SMS/MMS MCP Server
 A Model Context Protocol server for SMS and MMS messaging services.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "mBaaS Team"
 __email__ = "jjunmo@mbaas.kr"
 
